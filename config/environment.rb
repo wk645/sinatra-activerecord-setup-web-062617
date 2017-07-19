161c04b7f7bd4878eebddbd727b9193af68e637a
wk645 <wk645@nyu.edu>
2017-07-19 08:57:37 -0400

    setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+	set :database, 'sqlite3:db/database.db'
+	# database.db is the name of the db
+end
+
 require './app'
